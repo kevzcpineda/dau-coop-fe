21f7a3f86cf8511d87478c5cc29304ab6703ac15
kevzcpineda <kevinpineda098@gmail.com>
2023-11-08 13:18:37 +0800

    fix loan payments pdf

--- src/components/pdf/Paper.jsx
+++ src/components/pdf/Paper.jsx
@@ -6,7 +6,112 @@ const Paper = React.forwardRef((props, ref) => {
   return (
     <div style={{ display: 'none' }}>
       <div ref={ref} className='user_loan_container'>
-        <div className='header'>
+        <table className='report-container'>
+          <thead className='report-header'>
+            <tr>
+              <th className='report-header-cell'>
+                <div className='header-info'>
+                  <div>
+                    <img src={dauLogo} className='header_logo' />
+                  </div>
+                  <div>
+                    <h3>DAU OPERATORS AND DRIVERS TRANSPORT COOPERATIVE</h3>
+                    <p>
+                      # 7110 Room 7 RC Building Mc Arthur Hi-Way Dau Mabalacat
+                      City Pampanga
+                    </p>
+                    <p>CIN - 0102030090</p>
+                    <p>Registration / Confirmation No. MLA - C-046</p>
+                    <p>Dated March 19, 1991</p>
+                    <h3>JANUARY</h3>
+                  </div>
+                </div>
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className='report-content'>
+            <div className='details'>
+              <div className='sec1'>
+                <p>
+                  NAME:
+                  {` ${props?.user?.first_name} ${props?.user?.last_name}`}
+                </p>
+                <p>LOAN GRANTED: {` ${props?.user?.loan}`}</p>
+                <p>ADDRESS:{` ${props?.user?.address}`}</p>
+                <p>DAILY PAYMENT: none</p>
+                <p>DUE DATE: {props?.user?.penalty_date}</p>
+              </div>
+              <div className='sec2'>
+                <p>DATE DRANTED: {` ${props?.user?.date}`}</p>
+                <p>INTEREST:{` ${props?.user?.interest}`}</p>
+                <p>SERVICE FEE: {` ${props?.user?.service_fee}`}</p>
+                <p>TERM OF LOAN: 3</p>
+                <p>DATE LAST PAYMENT: none</p>
+              </div>
+            </div>
+            <div className='voucher_section'>
+              <p>VOUCHER NUMBER: {props?.user?.voucher_number}</p>
+              <p>CHECK NUMBER: {props?.user?.check_number}</p>
+              <p>
+                PROMISSORY NOTE NUMBER: {props?.user?.promissory_note_number}
+              </p>
+            </div>
+            <table className='loan_payments_table'>
+              <tr className='loan_payments_table_tr'>
+                <th className='loan_payments_table_th'>Date Payments</th>
+                <th className='loan_payments_table_th'>CR number</th>
+                <th className='loan_payments_table_th'>Loan granted</th>
+                <th className='loan_payments_table_th'>Interest</th>
+                <th className='loan_payments_table_th'>Service fee</th>
+                <th className='loan_payments_table_th'>Penalty</th>
+                <th className='loan_payments_table_th'>Daily Payment</th>
+                <th className='loan_payments_table_th'>Peyment</th>
+                <th className='loan_payments_table_th'>Loan balance</th>
+                <th className='loan_payments_table_th'>Take home</th>
+              </tr>
+              <tr className='loan_payments_table_tr'>
+                <td className='loan_payments_table_td'></td>
+                <td className='loan_payments_table_td'></td>
+                <td className='loan_payments_table_td'>{props?.user?.loan}</td>
+                <td className='loan_payments_table_td'>
+                  {props?.user?.interest}
+                </td>
+                <td className='loan_payments_table_td'>
+                  {props?.user?.service_fee}
+                </td>
+                <td className='loan_payments_table_td'>
+                  {props?.user?.penalty}
+                </td>
+                <td className='loan_payments_table_td'></td>
+                <td className='loan_payments_table_td'></td>
+                <td className='loan_payments_table_td'>
+                  {props?.user?.balance}
+                </td>
+                <td className='loan_payments_table_td'>
+                  {props?.user?.net_amount}
+                </td>
+              </tr>
+              {props?.loanUserPayments?.map((item) => {
+                return (
+                  <tr className='loan_payments_table_tr'>
+                    <td className='loan_payments_table_td'>{item.date}</td>
+                    <td className='loan_payments_table_td'>{item.ticket}</td>
+                    <td className='loan_payments_table_td'></td>
+                    <td className='loan_payments_table_td'></td>
+                    <td className='loan_payments_table_td'></td>
+                    <td className='loan_payments_table_td'></td>
+                    <td className='loan_payments_table_td'></td>
+                    <td className='loan_payments_table_td'>{item.amount}</td>
+                    <td className='loan_payments_table_td'></td>
+                    <td className='loan_payments_table_td'></td>
+                  </tr>
+                );
+              })}
+            </table>
+          </tbody>
+        </table>
+        {/* <div className='header'>
           <div>
             <img src={dauLogo} className='logo' />
           </div>
@@ -86,7 +191,7 @@ const Paper = React.forwardRef((props, ref) => {
               </tr>
             );
           })}
-        </table>
+        </table> */}
         {/* ref */}
       </div>
     </div>
